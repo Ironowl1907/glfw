67b9fc33757aa8fbadad111924d22c4c815e81f1
Ironowl <facuguialtana7@gmail.com>
2024-11-21 20:01:39 -0300

    Fixed premake

--- premake5.lua
+++ premake5.lua
@@ -8,51 +8,23 @@ targetdir("bin/%{prj.name}")
 objdir("bin-int/%{prj.name}")
 
 files({
-	"include/GLFW/glfw3.h",
-	"include/GLFW/glfw3native.h",
-	"src/glfw_config.h",
-	"src/context.c",
-	"src/init.c",
-	"src/input.c",
-	"src/monitor.c",
-
-	"src/null_init.c",
-	"src/null_joystick.c",
-	"src/null_monitor.c",
-	"src/null_window.c",
-
-	"src/platform.c",
-	"src/vulkan.c",
-	"src/window.c",
+	"src/**.cpp",
+	"src/**.h",
+	"/include/**.h",
 })
 
 filter("system:linux")
 pic("On")
-
 systemversion("latest")
-
 files({
-	"src/x11_init.c",
-	"src/x11_monitor.c",
-	"src/x11_window.c",
-	"src/xkb_unicode.c",
-	"src/posix_module.c",
-	"src/posix_time.c",
-	"src/posix_thread.c",
-	"src/posix_module.c",
-	"src/glx_context.c",
-	"src/egl_context.c",
-	"src/osmesa_context.c",
-	"src/linux_joystick.c",
+	"src/**.c",
 })
-
 defines({
 	"_GLFW_X11",
 })
 
 filter("system:macosx")
 pic("On")
-
 files({
 	"src/cocoa_init.m",
 	"src/cocoa_monitor.m",
@@ -65,14 +37,12 @@ files({
 	"src/osmesa_context.c",
 	"src/egl_context.c",
 })
-
 defines({
 	"_GLFW_COCOA",
 })
 
 filter("system:windows")
 systemversion("latest")
-
 files({
 	"src/win32_init.c",
 	"src/win32_joystick.c",
@@ -85,7 +55,6 @@ files({
 	"src/egl_context.c",
 	"src/osmesa_context.c",
 })
-
 defines({
 	"_GLFW_WIN32",
 	"_CRT_SECURE_NO_WARNINGS",
@@ -94,12 +63,6 @@ defines({
 filter("configurations:Debug")
 runtime("Debug")
 symbols("on")
-
-filter({ "system:windows", "configurations:Debug-AS" })
-runtime("Debug")
-symbols("on")
-sanitize({ "Address" })
-flags({ "NoRuntimeChecks", "NoIncrementalLink" })
 
 filter("configurations:Release")
 runtime("Release")
